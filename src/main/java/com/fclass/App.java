ecd22b08d7f960e099b696a4699e551243484c3d
Raffay Syed <raffayhusayn@gmail.com>
2021-11-18 08:01:42 -0500

    Add Book Class and write basic code to make a simple insert in the table that Hibernate makes for us automatically (because of our hibernate configurations)

--- src/main/java/com/fclass/App.java
+++ src/main/java/com/fclass/App.java
@@ -1,5 +1,14 @@
 package com.fclass;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration conf = new Configuration().configure().addAnnotatedClass(Book.class);
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        SessionFactory factory = conf.buildSessionFactory(serviceRegistry);
+        Session session = factory.openSession();
+
+        Book flowers4Algernon = new Book();
+        flowers4Algernon.setAuthor("Raffay");
+        flowers4Algernon.setIabn(0);
+        flowers4Algernon.setName("Flowers for Algernon");
+
+        Book theStranger = new Book();
+        theStranger.setAuthor("Albert Camus");
+        theStranger.setIabn(5);
+        theStranger.setName("The Stranger");
+
+        Transaction tx = session.beginTransaction();
+        session.save(flowers4Algernon);
+        tx.commit();
+        Transaction tx1 = session.beginTransaction();
+        session.save(theStranger);
+        tx1.commit();
+
     }
 }
--- src/main/java/com/fclass/Book.java
+++ src/main/java/com/fclass/Book.java
@@ -1,2 +1,36 @@
-package com.fclass;public class Book {
+package com.fclass;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    int iabn;
+    String name;
+    String author;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIabn(int iabn) {
+        this.iabn = iabn;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getIabn(){
+        return iabn;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
 }
