61a4c7b54c9a3d77deee01f3ebdb4a1855ec2d0c
Raffay Syed <raffayhusayn@gmail.com>
2021-11-19 23:37:58 -0500

    Add a way to store many to many relation in database using hibernate

--- src/main/java/com/fclass/App.java
+++ src/main/java/com/fclass/App.java
@@ -9,7 +9,9 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Hello world!
@@ -19,7 +21,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Configuration conf = new Configuration().configure().addAnnotatedClass(Book.class);
+        Configuration conf = new Configuration().configure().addAnnotatedClass(Book.class).addAnnotatedClass(Library.class);
         ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
         SessionFactory factory = conf.buildSessionFactory(serviceRegistry);
         Session session = factory.openSession();
@@ -35,6 +37,7 @@ public class App
         herbert.setAuthorAge(100);
         herbert.setBooks(Arrays.asList("herbert book 1", "herbert book 2", "herbert book 3"));
 
+
         Book flowers4Algernon = new Book();
         flowers4Algernon.setAuthor(raffay);
         flowers4Algernon.setId(0);
@@ -45,19 +48,38 @@ public class App
         theStranger.setId(5);
         theStranger.setName("The Stranger");
 
-        Book raffay_book = new Book();
+        Library nyLib  = new Library();
+        nyLib.setId(1001);
+        nyLib.setCity("New York");
+        nyLib.setName("New York Public Library");
+
+        Library queensLib  = new Library();
+        queensLib.setId(1002);
+        queensLib.setCity("Queens");
+        queensLib.setName("Queens Public Library");
+
+        //setting many to many relations list for library and books
+        theStranger.setLibraryList(Arrays.asList(nyLib,queensLib ));
+        flowers4Algernon.setLibraryList(Arrays.asList(nyLib,queensLib ));
+        nyLib.setBooks(Arrays.asList(flowers4Algernon, theStranger));
+        queensLib.setBooks(Arrays.asList(flowers4Algernon, theStranger));
+
+
+//        Book raffay_book = new Book();
 
         Transaction tx = session.beginTransaction();
         session.save(flowers4Algernon);
         session.save(theStranger);
-        raffay_book = session.get(Book.class , 0);
+        session.save(nyLib);
+        session.save(queensLib);
+//        raffay_book = session.get(Book.class , 0);
         tx.commit();
 
 
 //        Transaction tx1 = session.beginTransaction();
 //        tx1.commit();
 
-        System.out.println(raffay_book);
+//        System.out.println(raffay_book);
 
 
 
